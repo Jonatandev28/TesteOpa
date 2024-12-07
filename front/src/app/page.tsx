d96da55d8a483f73fb6fc310173f7bf1ecf707c5
Jonatandev28 <jonatan28.almeida@gmail.com>
2024-12-07 11:58:42 -0300

    ajustes na lógica de seção do usuario

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,22 +1,7 @@
-'use client'
-
-import useAuth from "@/hooks/useAuth";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { redirect } from "next/navigation"
 
 const index = () => {
-  const router = useRouter()
-  const { data } = useAuth()
-
-  useEffect(() => {
-    if (data) {
-      router.push('/chat')
-    } else {
-      router.push('/login')
-    }
-  }, [data])
-
-  return null
+  redirect("/login")
 }
 
 export default index
